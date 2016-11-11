c225e3d10caf2d0ab22ecd707e4517234e751627
Crystal Rae Bryant <crystal@crystalraebryant.com>
2016-11-11 12:50:33 -0600

    extracted routes

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ var mongoose          = require ('mongoose');
 var morgan            = require ('morgan');
 var bodyParser        = require('body-parser');
 var methodOverride    = require('method-override');
+var database          = require('./config/database');
 
 // configuration ====================================
-var database = require('./config/database');
-
 mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
@@ -18,55 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// routes ===========================================
-
-// api ---------------------------------------------
-// get all todos
-app.get('/api/todos', function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err)
-            res.send(err)
-        res.json(todos);
-    });
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
-    Todo.create({
-        text : req.body.text,
-        done: false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        });
-    });
-});
-
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        })
-    })
-})
-
-// application --------------------------------------
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+// routs ============================================
+require('./app/routes')(app);
 
 // listen (start app with node server.js) ===========
 app.listen(8080);
