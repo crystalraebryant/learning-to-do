6beb76fba5cddb425669018b47674a6d4dd20bbb
Crystal Rae Bryant <crystal@crystalraebryant.com>
2016-11-11 12:16:24 -0600

    setup angular controller and template

--- server.js
+++ server.js
@@ -17,29 +17,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// define mode ======================================
+// define model ======================================
 var Todo = mongoose.model('Todo', {
     text: String
 });
 
-// listen (start app with node server.js) ===========
-app.listen(8080);
-console.log("App listening on port 8080");
-
-// routes ============================================
+// routes ===========================================
 
 // api ---------------------------------------------
 // get all todos
 app.get('/api/todos', function(req, res) {
-Todo.find(function(err, todos) {
-      if (err)
-          res.send(err)
-      res.json(todos);
-  });
+    Todo.find(function(err, todos) {
+        if (err)
+            res.send(err)
+        res.json(todos);
+    });
 });
 
 // create todo and send back all todos after creation
-app.post('api/todos', function(req, res) {
+app.post('/api/todos', function(req, res) {
     Todo.create({
         text : req.body.text,
         done: false
@@ -56,7 +52,7 @@ app.post('api/todos', function(req, res) {
 });
 
 // delete a todo
-app.delete('api/todos/:todo_id', function(req, res) {
+app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
         _id : req.params.todo_id
     }, function(err, todo) {
@@ -70,3 +66,12 @@ app.delete('api/todos/:todo_id', function(req, res) {
         })
     })
 })
+
+// application --------------------------------------
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// listen (start app with node server.js) ===========
+app.listen(8080);
+console.log("App listening on port 8080");
